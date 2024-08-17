9c920c6282530b0a2bfdf85156a310604037a4a0
ThrasherLT <hydrauliclt@gmail.com>
2024-08-17 22:52:43 +0300

    Add custom merkle tree implementation
    The existing crates were too unflexible and most were impossible to get to work with the Poseidon hash function which is unavoidable for now, since sha256 support is pretty much broken in halo2_gadgets at this moment. The new Merkle tree turned out very small and highly flexible.

--- src/set_membership/mod.rs
+++ src/set_membership/mod.rs
@@ -1,5 +1,5 @@
 // pub mod zkp;
 
 // TODO doctests are forcing me to make some mods pub, so need to investigate how to keep them private.
-// mod poseidon_chip;
+pub mod merkle;
 pub mod poseidon_hasher;
