58ccda2b81badb3d9915d2eb503f803502e3341a
ThrasherLT <hydrauliclt@gmail.com>
2024-10-17 23:47:37 +0300

    Fix mock authority issues
    Since an early prototype of the client exists, it is now possible to test the mock_authority and fix some issues that were found. Also add saving and loading of blind signature keys.

--- src/bin/mock_authority.rs
+++ src/bin/mock_authority.rs
@@ -1,25 +1,90 @@
 //! This is a mock of the election authorities servers which will be responsible for ensuring the
 //! eligibility of the voters by signing their public keys. This is only used for testing purposes.
 
-// TODO test this out.
+use std::io::Write;
 
 use actix_web::{get, post, routes, web, App, HttpResponse, HttpServer, Responder};
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
-use crypto::signature::blind_sign::{BlindSignature, BlindSigner, BlindedMessage};
+use crypto::signature::blind_sign::{self, BlindSigner};
 use digital_voting::logging::start_logger;
-use digital_voting::Timestamp;
 use serde::{self, Deserialize, Serialize};
-use tracing::info;
 
 #[derive(Parser, Clone, Debug)]
 pub struct Args {
     #[clap(short = 'a', long = "address", default_value = "127.0.0.1:8081")]
     pub addr: std::net::SocketAddr,
+    #[clap(short = 'k', long = "new-keys", default_value_t = false)]
+    pub new_keys: bool,
 }
 
 struct AppState {
     blind_signer: BlindSigner,
+}
+
+/// The command that the node should execute on startup.
+#[derive(Parser, Clone, Debug)]
+pub enum Cmd {
+    /// Start a new blockchain and create a genesis block.
+    #[clap(about = "Blindly sign input message")]
+    Authenticate {
+        
+    },
+    /// Connect to an existing node of an existing blockchain.
+    #[clap(about = "Print public key for blind signature")]
+    GetPubKey {},
+}
+
+fn new_blind_signer(path: &str) -> Result<BlindSigner> {
+    let blind_signer = BlindSigner::new()?;
+    let mut blind_signer_cfg_file = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path)?;
+
+    writeln!(blind_signer_cfg_file, "{}", blind_signer.get_public_key()?)?;
+    writeln!(blind_signer_cfg_file, "{}", blind_signer.get_secret_key()?)?;
+
+    Ok(blind_signer)
+}
+
+fn setup_blind_signer(arg_new_keys: bool) -> Result<BlindSigner> {
+    let blind_signer_cfg_path = "authority-blind-signer-cfg";
+
+    if arg_new_keys {
+        if let Err(err) = std::fs::remove_file(blind_signer_cfg_path) {
+            // It's not an error if the file actually doesn't exist, since we're deleting it anyway.
+            if err.kind() != std::io::ErrorKind::NotFound {
+                anyhow::bail!("Failed to delete old blind signer cfg file: {}", err);
+            }
+        }
+    }
+
+    match load_blind_signer_from_fs(blind_signer_cfg_path) {
+        Ok(blind_signer) => Ok(blind_signer),
+        Err(_) => Ok(new_blind_signer(blind_signer_cfg_path)?),
+    }
+}
+
+fn load_blind_signer_from_fs(path: &str) -> Result<BlindSigner> {
+    if std::path::Path::new(path).exists() {
+        let blind_signer_cfg = std::fs::read_to_string(path)?;
+        let mut blind_signer_cfg = blind_signer_cfg.lines().take(2);
+        let (pk, sk) = (
+            blind_signer_cfg
+                .next()
+                .ok_or(anyhow!("Failed to parse blind signer public key"))?
+                .parse()?,
+            blind_signer_cfg
+                .next()
+                .ok_or(anyhow!("Failed to parse blind signer secret key"))?
+                .parse()?,
+        );
+        Ok(BlindSigner::new_from_keys(pk, sk)?)
+    } else {
+        Err(anyhow!("Blind signer config not found"))
+    }
 }
 
 #[tokio::main]
@@ -27,15 +92,18 @@ async fn main() -> Result<()> {
     let _tracing_worker_guard = start_logger("mock_authority.log")?;
     let args = Args::parse();
 
-    println!("Starting mock authority on: {}...", args.addr);
+    println!("Starting mock authority server on: {}...", args.addr);
+
+    let blind_signer = setup_blind_signer(args.new_keys)?;
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(AppState {
-                blind_signer: BlindSigner::new().expect("Could not create a blind signer"),
+                // TODO expect:
+                blind_signer: blind_signer.clone(),
             }))
             .service(greet)
-            .service(verify)
+            .service(authenticate)
             .service(get_pkey)
     })
     .bind(args.addr)?
@@ -49,52 +117,32 @@ async fn main() -> Result<()> {
 #[get("/")]
 #[get("/index.html")]
 async fn greet() -> impl Responder {
-    "Hello! Please send a POST request to /verify with a JSON body, containing a public key, a vote, some mock authentication data, and a signature.\n"
+    "Hello! Please send a POST request to /authenticate with a JSON body, containing a public key, a vote, some mock authentication data, and a signature.\n"
 }
 
-#[post("/verify")]
-pub async fn verify(
+#[post("/authenticate")]
+pub async fn authenticate(
     verification_request: web::Json<VerificationRequest>,
     data: web::Data<AppState>,
 ) -> impl Responder {
-    info!("POST: /vote {verification_request:?}");
-    if mock_verify_authentication_data(verification_request.authentication_data.as_str()) {
-        match data
-            .blind_signer
-            .bling_sign(&verification_request.pkey.clone())
-        {
-            Ok(blind_signature) => {
-                HttpResponse::Ok().json(VerificationResponse::Verified { blind_signature })
-            }
-            Err(e) => HttpResponse::InternalServerError().body(format!("Error: {e}")),
-        }
-    } else {
-        HttpResponse::BadRequest().json(VerificationResponse::Denied)
+    match data
+        .blind_signer
+        .bling_sign(&verification_request.blinded_pkey)
+    {
+        Ok(blind_signature) => HttpResponse::Ok().json(blind_signature),
+        Err(e) => HttpResponse::InternalServerError().body(format!("Error: {e}")),
     }
 }
 
 #[get("/pkey")]
 pub async fn get_pkey(data: web::Data<AppState>) -> impl Responder {
     match data.blind_signer.get_public_key() {
-        Ok(pkey) => HttpResponse::Ok().json(pkey),
+        Ok(pkey) => HttpResponse::Ok().json(pkey.to_string()),
         Err(e) => HttpResponse::InternalServerError().body(format!("Error: {e}")),
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct VerificationRequest {
-    pkey: BlindedMessage,
-    authentication_data: String,
-    timestamp: Timestamp,
-    signature: Vec<u8>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum VerificationResponse {
-    Verified { blind_signature: BlindSignature },
-    Denied,
-}
-
-fn mock_verify_authentication_data(authentication_data: &str) -> bool {
-    !authentication_data.is_empty()
+    blinded_pkey: blind_sign::BlindedMessage,
 }
